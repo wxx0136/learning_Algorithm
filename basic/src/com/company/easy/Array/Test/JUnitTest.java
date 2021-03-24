4bdf07ab88d4c7617d019cb43a83af877c90f411
wxx <wxx0136@gmail.com>
2021-03-23 20:42:17 -0500

    Init

---
+++ basic/src/com/company/easy/Array/Test/JUnitTest.java
@@ -0,0 +1,33 @@
+package com.company.easy.Array.Test;
+
+import com.company.easy.Array.MaxProfit;
+import com.company.easy.Array.RemoveDuplicates;
+import com.company.easy.Array.RotateArray;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JUnitTest {
+
+    @Test
+    public void RemoveDuplicates_Test() {
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/727/
+        RemoveDuplicates removeDuplicates = new RemoveDuplicates();
+        System.out.println(removeDuplicates.removeDuplicates(new int[]{0, 0, 1, 1, 1, 2, 2, 3, 3, 4}));
+    }
+
+    @Test
+    public void MaxProfit_Test() {
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/564/
+        MaxProfit maxProfit = new MaxProfit();
+        System.out.println(maxProfit.maxProfit(new int[]{7, 6, 4, 3, 1}));
+    }
+
+    @Test
+    public void RotateArray_Test() {
+        //https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/646/
+        RotateArray rotateArray = new RotateArray();
+        rotateArray.rotate(new int[]{1, 2, 3, 4, 5, 6, 7}, 3);
+    }
+
+}
