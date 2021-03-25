204c3d8728c8c5a69d49d7d4f0be207ab5960da4
wxx <wxx0136@gmail.com>
2021-03-25 12:03:31 -0500

    Learning

--- basic/src/com/easy/Array/MoveZeros.java
+++ basic/src/com/easy/Array/MoveZeros.java
@@ -4,18 +4,15 @@ import java.util.Arrays;
 
 public class MoveZeros {
     public void moveZeroes(int[] nums) {
-        int l = nums.length;
-        if (l == 0) return;
-        int j = 0;
-        for (int i = 0; i < l; i++) {
-            if (nums[i] != 0) {
-                nums[j] = nums[i];
-                j++;
-            }
+        if (nums == null || nums.length == 0) return;
+
+        int insertPos = 0;
+        for (int num : nums) {
+            if (num != 0) nums[insertPos++] = num;
         }
-        for (int x = j; x < l; x++) {
-            nums[x] = 0;
+
+        while (insertPos < nums.length) {
+            nums[insertPos++] = 0;
         }
-        System.out.println(Arrays.toString(nums));
     }
 }
